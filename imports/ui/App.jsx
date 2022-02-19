4437500a4bb122174caa4ceb5200bb23c8b4d729
you <you@example.com>
2022-02-19 14:56:00 +0800

    a

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,7 +4,7 @@ import {ContactList} from "./ContactList"
 
 export const App = () => (
   <div>
-    <h1>Meteor Wallet!</h1>
+    <h1>UK Wallet!</h1>
     <ContactForm></ContactForm>
       <ContactList/>
   </div>
